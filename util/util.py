983d1e5eef63534311c7bbde22f2c8c2ee9a3e76
Kaycee <nwosukelechi90@gmail.com>
2023-05-23 22:08:34 +0100

    fixing the grayscale issue

--- util/util.py
+++ util/util.py
@@ -49,6 +49,7 @@ def tensor2im(input_image, imtype=np.uint8):
 
     print("image_numpy shape:", image_numpy.shape)
     print("specified axes:", (1, 2, 0))
+
     if image_numpy.ndim == 2:  # Grayscale image
         image_numpy = np.expand_dims(image_numpy, axis=-1)  # Add single channel dimension
         image_numpy = np.repeat(image_numpy, 3, axis=-1)  # Repeat single channel across all three channels
@@ -59,7 +60,7 @@ def tensor2im(input_image, imtype=np.uint8):
     image_numpy = (image_numpy - image_numpy.min()) / (image_numpy.max() - image_numpy.min())  # Rescale to [0, 1]
     image_numpy = (image_numpy * 255).astype(np.uint8)  # Convert to [0, 255]
 
-    return image_numpy
+    return image_numpy.astype(imtype)
 
 
 def diagnose_network(net, name='network'):
