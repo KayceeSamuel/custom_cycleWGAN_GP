2c09296b042b857f36ca44042d236aa296332ded
Kaycee <nwosukelechi90@gmail.com>
2023-05-24 21:59:58 +0100

    fixing grayscale issue

--- util/visualizer.py
+++ util/visualizer.py
@@ -170,7 +170,8 @@ class Visualizer():
             # save images to the disk
             for label, image in visuals.items():
                 for idx, image in enumerate(image):
-                    print('Checking the shape for the grayscale', image.shape)  # print shape before tensor2im
+                    print("Min value in tensor:", image.min())  # Add these two lines
+                    print("Max value in tensor:", image.max())
                     image_numpy = util.tensor2im(image)
                     img_path = os.path.join(self.img_dir, 'epoch%.3d_%s_%d.png' % (epoch, label, idx))
                     print("Shape of image_numpy:", image_numpy.shape)  # Add these two lines
